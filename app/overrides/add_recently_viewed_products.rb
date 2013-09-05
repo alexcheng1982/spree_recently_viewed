f97dfb22f97dafa8cd1d7537c68d8d56a12a4d4f
Fu Cheng <alexcheng1982@gmail.com>
2013-09-05 21:36:09 +1200

    update style

--- app/overrides/add_recently_viewed_products.rb
+++ app/overrides/add_recently_viewed_products.rb
@@ -1,7 +1,9 @@
+=begin
 Deface::Override.new(:virtual_path => 'spree/shared/_products',
                      :name => 'add_recently_viewed_products_to_products_index',
                      :insert_after => "#products[data-hook], [data-hook='products']",
                      :partial => 'spree/shared/recently_viewed_products')
+=end
 
 Deface::Override.new(:virtual_path => 'spree/products/show',
                      :name => 'add_recently_viewed_products_to_products_show',
